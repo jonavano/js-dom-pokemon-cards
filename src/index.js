bcb83922a50183f9ccbd77d1c13bca0eea4bfebd
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-08-27 15:02:54 +0000

    add game data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import {data } from './src/data.js';
 
-console.log(data);
+// console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
@@ -11,35 +11,46 @@ createAllCards()
 
 
 function createAllCards() {
-    for (let i = 0; i< data.length; i++) {
-        createOneCard(i)
+    for (const pokemon of data) {
+        createOneCard(pokemon)
     }
 }
-function createOneCard(index) {
-    console.log("ran ");
+function createOneCard(pokemon) {
+    // console.log("ran ");
     const cardLi = document.createElement("li")
     cardLi.className = "card"
     // console.log("ran 23");
     
     const cardTitle = document.createElement('h2')
     cardTitle.className = "card--title"
-    cardTitle.innerText = data[index].name
+    cardTitle.innerText = pokemon.name
 
     const image = document.createElement('img')
     image.className = "card--img"
     image.width = "256"
-    image.src = data[index].sprites.front_default
+    image.src = pokemon.sprites.front_default
 
     const cardText = document.createElement("ul")
     cardText.className = "card--text"
 
-    for (const stat of data[0].stats){
-        console.log(stat)
-        console.log(stat.base_stat)
+    for (const stat of pokemon.stats){
+        // console.log(stat)
+        // console.log(stat.base_stat)
         const cardStat = document.createElement("li")
-        cardStat.innerText = (stat.stat.name.toUpperCase()) + ":" + stat.base_stat
+        cardStat.innerText = (stat.stat.name.toUpperCase()) + ": " + stat.base_stat
         cardText.appendChild(cardStat)
     }
+
+    const gameStat = document.createElement('li')
+    gameStat.innerText += "GAMES: "
+
+    for (const games of pokemon.game_indices) {
+        gameStat.innerText += games.version.name + ", "
+    
+    }
+    
+    cardText.appendChild(gameStat)
+
 
     cardLi.appendChild(cardTitle);
     cardLi.appendChild(image)
